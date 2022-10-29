67b74e6ed2f50ff35c707e694b4edd271e1f5329
Lemit <Mihtrop2005@yandex.ru>
2022-10-29 21:24:03 +0300

    first commit

---
+++ src/main/java/com/example/mypr/MyPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.mypr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPrApplication.class, args);
+	}
+
+}
